4317e7f9911fe7816565e350139ace106a97afeb
cj <timminsjc@gmail.com>
2020-02-04 14:42:40 +0000

    Added database for use in coursework Added city class Added code to App.java to test database

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,15 @@
+package com.napier.sem;
+
+public class City {
+
+    public int id;
+
+    public String name;
+
+    public int country_code;
+
+    public String district;
+
+    public int population;
+
+}
