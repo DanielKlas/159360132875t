baee64168a8449a7f0ff70613fad49900fdf5942
cj <timminsjc@gmail.com>
2020-02-07 14:28:28 +0000

    Refactored database connection into a new class(DBConnection) Removed code pertaining to City data and querying the database as it was only intended for example usage New DBConnection class is a singleton that manages our apps connection to the mariaDB server

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -6,164 +6,20 @@ import java.util.List;
 
 public class App
 {
+    public static DBConnection dbConnection = DBConnection.getInstance();
     public static void main(String[] args)
     {
             // Create new Application
             App a = new App();
 
             // Connect to database
-            a.connect();
+            dbConnection.connect();
 
-            City city = a.getCity(1);
-            a.displayCity(city);
-            ArrayList<City> cities= a.getAllCities();
-            if (cities!=null) {
-                for (City c : cities) {
-                    a.displayCity(c);
-                }
-            }
-            else{
-                System.out.println("cities is null");
-            }
             // Disconnect from database
-            a.disconnect();
+            dbConnection.disconnect();
 
     }
-    /**
-     * Connection to MariaDB database.
-     */
-    private Connection con = null;
 
-    /**
-     * Connect to the MariaDB database.
-     */
-    public void connect()
-    {
-        try
-        {
-            // Load Database driver
-            Class.forName("org.mariadb.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
 
-        int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
-            System.out.println("Connecting to database...");
-            try
-            {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mariadb://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
-                System.out.println("Thread interrupted? Should not happen.");
-            }
-        }
-    }
 
-    /**
-     * Disconnect from the MariaDB database.
-     */
-    public void disconnect() {
-        if (con != null) {
-            try {
-                // Close connection
-                con.close();
-            } catch (Exception e) {
-                System.out.println("Error closing connection to database");
-            }
-        }
-    }
-
-    public ArrayList<City> getAllCities()
-    {
-        ArrayList<City> cities = new ArrayList<City>();
-        try
-        {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT * "
-                            + "FROM city ";
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Return new employee if valid.
-            // Check one is returned
-            while(rset.next())
-            {
-                City city = new City();
-                city.id = rset.getInt("ID");
-                city.name = rset.getString("Name");
-                city.country_code=rset.getString("CountryCode");
-                city.district=rset.getString("District");
-                city.population=rset.getInt("Population");
-                cities.add(city);
-            }
-            return cities;
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get city details");
-            return null;
-        }
-    }
-    public City getCity(int no)
-    {
-        try
-        {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT ID, Name, CountryCode, District, Population "
-                            + "FROM city " + "WHERE ID = " + no;
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Return new employee if valid.
-            // Check one is returned
-            if (rset.next())
-            {
-                City city = new City();
-                city.id = rset.getInt("ID");
-                city.name = rset.getString("Name");
-                city.country_code=rset.getString("CountryCode");
-                city.district=rset.getString("District");
-                city.population=rset.getInt("Population");
-                return city;
-            }
-            else
-                return null;
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get city details");
-            return null;
-        }
-    }
-
-    public void displayCity(City city) {
-        if (city != null)
-        {
-            System.out.println(city.id + " , " + city.name + " , " +
-                    city.country_code + " , " + city.district +
-                    " , " + city.population);
-        }
-    }
 }
