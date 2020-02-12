338e3c94b7260dda89c24977f60df146d1faf4bd
Connor Timmins <timminsjc@gmail.com>
2020-02-12 14:40:53 +0000

    Added basic CLI frontend Added queries and respective display functions Updated docker-compose.yml to allow for an attached console

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,9 @@
 package com.napier.sem;
 
 import javax.swing.plaf.nimbus.State;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,44 +11,104 @@ import java.util.List;
 public class App
 {
     public static DBConnection dbConnection = DBConnection.getInstance();
+
     public static void main(String[] args) {
         // Create new Application
         App a = new App();
 
         // Connect to database
         dbConnection.connect();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        PopReportGenerator popReportGenerator= new PopReportGenerator();
-        String continentReport = popReportGenerator.getQuery("Continent", "'Asia'");
-        System.out.println(continentReport);
-        try {
-            Statement statement = dbConnection.con.createStatement();
-            ResultSet rset = statement.executeQuery(continentReport);
-            rset.next();
-            String name = rset.getString("Continent");
-            long totalPop = rset.getLong("Total Population");
-            long urbanPop = rset.getLong("Urban Population");
-            float urbanPercent = rset.getFloat("UrbanPercent");
-            long ruralPop =rset.getLong("Rural Population");
-            float RuralPercent = rset.getFloat("RuralPercent");
-            System.out.println("Continent: " + name + "\n");
-            System.out.println("Total Pop: " + totalPop + "\n");
-            System.out.println("Urban Pop: " + urbanPop + "\n");
-            System.out.println("Urban Percent: " + urbanPercent + "\n");
-            System.out.println("Rural Pop: " + ruralPop + "\n");
-            System.out.println("Rural Percent: " + RuralPercent + "\n");
+        while (true) {
+            System.out.println("Please select a report type");
+            System.out.println("1. Population");
+            System.out.println("0: Exit");
+            try {
+                String input = reader.readLine();
+                System.out.println();
+                switch (input) {
+                    case "1":
+                        PopReportMenu();
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        System.out.println("Invalid input");
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
         }
-        catch (Exception e)
-        {
-            System.out.println("SQL broken");
-            e.printStackTrace();
-        }
-
         // Disconnect from database
         dbConnection.disconnect();
-
     }
 
+    private static void PopReportMenu()
+    {
+        PopReportGenerator popReportGenerator = new PopReportGenerator();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        while (true) {
+            System.out.println("Please select a report type");
+            System.out.println("1. World");
+            System.out.println("2. Continent");
+            System.out.println("3. Region");
+            System.out.println("4. Country");
+            System.out.println("5. District");
+            System.out.println("6. City");
+            System.out.println("0: Exit");
 
+            try {
+                String input = reader.readLine();
+                System.out.println();
+                switch (input) {
+                    case "1":
+                        String worldReport = popReportGenerator.getWorldQuery();
+                        System.out.println(worldReport);
+                        break;
+                    case "2":
+                        System.out.println("Please enter a continent" + "\n");
+                        String continent = reader.readLine();
+                        String continentReport = popReportGenerator.getGeneralQuery("Continent",continent);
+                        System.out.println(continentReport);
+                        break;
+                    case "3":
+                        System.out.println("Please enter a region" + "\n");
+                        String region = reader.readLine();
+                        String regionReport = popReportGenerator.getGeneralQuery("Region",region);
+                        System.out.println(regionReport);
+                        break;
+                    case "4":
+                        System.out.println("Please enter a country" + "\n");
+                        String country = reader.readLine();
+                        String countryReport = popReportGenerator.getGeneralQuery("Name", country);
+                        System.out.println(countryReport);
+                        break;
+                    case "5":
+                        System.out.println("Please enter a district" + "\n");
+                        String district = reader.readLine();
+                        String districtReport = popReportGenerator.getDistrictQuery(district);
+                        System.out.println(districtReport);
+                        break;
+                    case "6":
+                        System.out.println("Please enter a city" + "\n");
+                        String city = reader.readLine();
+                        String cityReport = popReportGenerator.getCityQuery(city);
+                        System.out.println(cityReport);
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        System.out.println("Invalid input");
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
 }
