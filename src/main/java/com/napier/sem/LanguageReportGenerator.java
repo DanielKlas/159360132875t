b32f02b80412a8fecf4b61061ef6cd11fbfd37bb
Connor Timmins <timminsjc@gmail.com>
2020-02-18 10:53:54 +0000

    Fixed typo in SQL query Added while loop to displayLanguagesReport to support multiple results

--- src/main/java/com/napier/sem/LanguageReportGenerator.java
+++ src/main/java/com/napier/sem/LanguageReportGenerator.java
@@ -6,7 +6,7 @@ public class LanguageReportGenerator {
     public String getLanguagesQuery(String name)
     {
         String string = "SELECT countrylanguage.Language AS 'Language spoken', SUM(country.Population) AS 'Population', ((SUM(country.Population) / 6078749450) * 100) AS 'Percent of the world' "
-                + "FROM country JOIN countrylanguage ON country.Code = countrylanguage.CountryCode"
+                + "FROM country JOIN countrylanguage ON country.Code = countrylanguage.CountryCode "
                 + "WHERE countrylanguage.Language IN ('Chinese','English','Hindi','Spanish','Arabic')"
                 + "GROUP BY countrylanguage.Language ORDER BY SUM(country.Population) DESC;";
         String format = String.format(string,name);
@@ -15,16 +15,17 @@ public class LanguageReportGenerator {
 
     private String displayLanguagesReport(String report, String name)
     {
-        String display=null;
+        String display="";
         try {
             DBConnection dbConnection = DBConnection.getInstance();
             Statement statement = dbConnection.con.createStatement();
             ResultSet rset = statement.executeQuery(report);
-            rset.next();
-            String languageSpoken = rset.getString("Language spoken");
-            int population = rset.getInt("Population");
-            int percentage = rset.getInt("Percent of the world");
-            display = "Language spoken: " + languageSpoken + "Population of speakers : " + population + "Percent of the world: " + percentage;
+            while (rset.next()) {
+                String languageSpoken = rset.getString("Language spoken");
+                int population = rset.getInt("Population");
+                int percentage = rset.getInt("Percent of the world");
+                display += "Language spoken: " + languageSpoken + " Population of speakers : " + population + " Percent of the world: " + percentage + "\n";
+            }
         }
         catch (Exception e)
         {
