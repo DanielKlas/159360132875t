75102fefcf1dbe386d09ea842a132e95eb7eef50
Connor Timmins <timminsjc@gmail.com>
2020-03-16 22:11:35 +0000

    Missing argument fixed

--- src/test/java/com/napier/sem/MyTest.java
+++ src/test/java/com/napier/sem/MyTest.java
@@ -9,15 +9,15 @@ class MyTest
     @Test
     void DBConnectionTest()
     {
-        DBConnection connection = DBConnection.getInstance("db::3306");
-        Boolean Result = connection.connect();
+        DBConnection connection = DBConnection.getInstance();
+        Boolean Result = connection.connect("db::3306");
         assertTrue(Result);
     }
 
     @Test
     void DBdisconnect(){
-        DBConnection connection = DBConnection.getInstance("db::3306");
-        connection.connect();
+        DBConnection connection = DBConnection.getInstance();
+        connection.connect("db::3306");
         Boolean Result = connection.disconnect();
         assertTrue(Result);
     }
