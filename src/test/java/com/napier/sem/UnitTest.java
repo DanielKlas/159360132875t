c3746c1d9c585feb0aed117a17ac8b4314227a07
Connor Timmins <timminsjc@gmail.com>
2020-03-17 12:25:40 +0000

    Fixed missing argument

--- src/test/java/com/napier/sem/UnitTest.java
+++ src/test/java/com/napier/sem/UnitTest.java
@@ -1,3 +1,5 @@
+package com.napier.sem;
+
 import com.napier.sem.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,15 +8,13 @@ import java.util.regex.Pattern;
 
 public class UnitTest {
 
-
-
     class MyTest
     {
         @Test
         void DBConnectionTest()
         {
             DBConnection connection = DBConnection.getInstance();
-           Boolean Result = connection.connect();
+           Boolean Result = connection.connect("localhost:33060");
             assertTrue(Result);
         }
 
